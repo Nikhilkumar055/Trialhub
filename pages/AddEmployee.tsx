c8f9cfb9aba21727cd6ae155437d792519b01cac
Vivek Srivastava <88632897+Vivek3072@users.noreply.github.com>
2022-12-28 12:19:17 +0530

    Add Employee Form

---
+++ pages/AddEmployee.tsx
@@ -0,0 +1,12 @@
+import Header from './components/header'
+import React from 'react'
+import AddEmployeeForm from './components/employee/AddEmployeeForm.js'
+
+export default function AddEmployee() {
+  return (
+    <>
+    <Header />
+    <AddEmployeeForm />
+    </>
+  )
+}
