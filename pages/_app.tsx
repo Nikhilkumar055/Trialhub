82a40227c5c142aec8e4c7a9b0e666efa8b391ed
Shazeb Khan <shazebk.ug21.cs@nitp.ac.in>
2023-01-10 21:59:53 +0530

    Navbar and Attendance Page integrated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,17 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 import Layout from "../components/Layout";
+import Attendance from "./components/Attendance/Attendance";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+    <Attendance/>
+    </>
+ 
+    // <Layout>
+    //   <Component {...pageProps} />
+    // </Layout>
   );
 }
 
--- pages/attendence.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function attendence() {
-  return (
-    <div>attendence</div>
-  )
-}
-
-export default attendence
